de67519864957d11df7eaea429cc05173b04571e
William <35743604+GreenSoap@users.noreply.github.com>
2019-10-03 18:55:42 +0200

    Add files via upload

--- cfnscraper.bat
+++ cfnscraper.bat
@@ -4,7 +4,5 @@ rename config.ini config.bat
 call config.bat
 rename config.bat config.ini
 
-color 0C
 title CFN Scraper - %profile%
-casperjs --web-security=no scraper-min.js --mode="%mode%" --profile="%profile%" --steamid="%steamid%" --steampass="%steampass%  --refreshinterval=%refreshinterval% --writejson=%writejson% --jsonlocation=%jsonlocation%
-pause
+start "CFN Scraper - %profile%" casperjs/bin/casperjs.exe --web-security=no scraper-min.js --profile="%profile%" --steamid="%steamid%" --steampass="%steampass%  --refreshinterval=%refreshinterval% --writejson=%writejson% --jsonlocation=%jsonlocation%
