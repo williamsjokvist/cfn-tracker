9b185b044b857058c4e3341a8bda8c7ff0b69396
William <35743604+GreenSoap@users.noreply.github.com>
2023-02-21 01:22:43 +0100

    Updated title and favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
-// https://astro.build/config
-
-// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
