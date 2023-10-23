27a21559aa0dfd7069e8873ad1418a529468e346
William Sjökvist <35743604+williamsjokvist@users.noreply.github.com>
2023-10-23 21:36:18 +0200

    fix: error msg not rendering (#30)

--- gui/src/main/error-message.tsx
+++ gui/src/main/error-message.tsx
@@ -1,53 +1,48 @@
-import { Icon } from "@iconify/react"
-import clsx from "clsx"
-import React from "react"
+import { Icon } from "@iconify/react";
+import clsx from "clsx";
+import { useAnimate } from "framer-motion";
+import React from "react";
 
-const ErrorMessageContext = React.createContext<{
-  errorMessage: string|null ,
-  setErrorMessage: (message: string) => void
-}>({
-  errorMessage: null,
-  setErrorMessage: () => {}
-})
+type ErrorMessageProps = {
+  errorMessage?: string;
+  onFadedOut: () => void;
+};
 
-export const useErrorMessage = () => React.useContext(ErrorMessageContext)
-
-export const ErrorMessageProvider: React.FC<React.PropsWithChildren> = ( { children }) => {
-  const [_, setErrorMessage] = React.useState<string | null>(null)
-
-  return (
-    <ErrorMessageContext.Provider value={{ errorMessage: null, setErrorMessage }}>
-      {children}
-    </ErrorMessageContext.Provider>
-  )
-}
-
-export const ErrorMessage: React.FC = () => {
-  const [isOpen, setOpen] = React.useState(false);
-  const { errorMessage } = useErrorMessage()
+export const ErrorMessage: React.FC<ErrorMessageProps> = ({
+  errorMessage,
+  onFadedOut,
+}) => {
+  const [scope, animate] = useAnimate();
 
   React.useEffect(() => {
-    if (errorMessage) {
-      setOpen(true);
-      setTimeout(() => setOpen(false), 3500);
+    if (!errorMessage) {
+      return;
     }
+
+    animate("#error-message", { opacity: [0, 1] }).then(() => {
+      animate("#error-message", { opacity: [1, 0] }, { delay: 3.5 }).then(() =>
+        onFadedOut()
+      );
+    });
   }, [errorMessage]);
 
   return (
-    <div
-      className={clsx(
-        "flex gap-8 items-center justify-around",
-        "fixed right-0 bottom-2 z-50",
-        "px-8 py-3 rounded-l-2xl text-xl backdrop-blur-sm pointer-events-none",
-        "bg-[rgba(255,0,0,.125)] transition-opacity",
-        `${isOpen ? `opacity-100` : `opacity-0`}`
-      )}
-    >
-      <Icon
-        icon="material-symbols:warning-outline"
-        className="text-[#ff6388] w-8 h-8 blink-pulse"
-      />
-      <span>{errorMessage}</span>
+    <div ref={scope} className="absolute">
+      <div
+        id="error-message"
+        className={clsx(
+          "flex gap-6 items-center justify-around",
+          "fixed bottom-2 z-50",
+          "px-8 py-3 rounded-r-xl text-xl backdrop-blur-sm pointer-events-none",
+          "bg-[rgba(255,0,0,.125)]"
+        )}
+      >
+        <Icon
+          icon="material-symbols:warning-outline"
+          className="text-[#ff6388] w-8 h-8 blink-pulse"
+        />
+        <span>{errorMessage}</span>
+      </div>
     </div>
   );
 };
