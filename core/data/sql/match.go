73139bedf0790a9593567985859605f8ee4bc673
William Sjökvist <william.sjokvist@gmail.com>
2023-10-24 19:45:15 +0200

    skeleton implementation

---
+++ core/data/sql/match.go
@@ -0,0 +1,39 @@
+package sql
+
+type Match struct {
+	Id                int    `db:"id"`
+	SessionId         string `db:"sessionId"`
+	Character         string `db:"character"`
+	LP                int    `db:"lp"`
+	MR                int    `db:"mr"`
+	Opponent          string `db:"opponent"`
+	OpponentCharacter string `db:"opponentCharacter"`
+	OpponentLP        int    `db:"opponentLP"`
+	OpponentMR        int    `db:"opponentMR"`
+	OpponentLeague    string `db:"opponentLeague"`
+	Victory           bool   `db:"victory"`
+	DateTime          string `db:"dateTime"`
+}
+
+type MatchStorage interface {
+	createMatchesTable() error
+	GetMatches(sessionId string) ([]*Match, error)
+	SaveMatch() error
+	RemoveMatches(sessionId string) error
+}
+
+func (s *Storage) GetMatches(sessionId string) ([]*Match, error) {
+	return nil, nil
+}
+
+func (s *Storage) SaveMatch() error {
+	return nil
+}
+
+func (s *Storage) RemoveMatches(sessionId string) error {
+	return nil
+}
+
+func (s *Storage) createMatchesTable() error {
+	return nil
+}
