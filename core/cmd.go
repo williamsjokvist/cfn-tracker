749149a8827f4759ab0ec4214879ebca3654f35f
William Sjökvist <william.sjokvist@gmail.com>
2023-10-24 19:45:15 +0200

    skeleton implementation

--- core/cmd.go
+++ core/cmd.go
@@ -31,12 +31,12 @@ type CommandHandler struct {
 	ctx     context.Context
 	tracker GameTracker
 	browser *shared.Browser
-	*data.CFNTrackerRepository
+	repo    *data.CFNTrackerRepository
 }
 
 func NewCommandHandler(trackerRepo *data.CFNTrackerRepository) *CommandHandler {
 	return &CommandHandler{
-		CFNTrackerRepository: trackerRepo,
+		repo: trackerRepo,
 	}
 }
 
@@ -78,7 +78,7 @@ func (ch *CommandHandler) GetMatchLog(cfn string) []data.TrackingState {
 }
 
 func (ch *CommandHandler) GetAvailableLogs() []data.User {
-	users, err := ch.CFNTrackerRepository.GetUsers(ch.ctx)
+	users, err := ch.repo.GetUsers(ch.ctx)
 	if err != nil {
 		fmt.Println(err)
 		return nil
@@ -113,7 +113,7 @@ func (ch *CommandHandler) ExportLogToCSV(cfn string) {
 func (ch *CommandHandler) SelectGame(game string) {
 	switch game {
 	case GameTypeSF6.String():
-		ch.tracker, _ = MakeSF6Tracker(ch.ctx, ch.browser, CapIDEmail, CapIDPassword, ch.CFNTrackerRepository)
+		ch.tracker, _ = MakeSF6Tracker(ch.ctx, ch.browser, CapIDEmail, CapIDPassword, ch.repo)
 	case GameTypeSFV.String():
 		ch.tracker, _ = MakeSFVTracker(ch.ctx, ch.browser, SteamUsername, SteamPassword)
 	}
