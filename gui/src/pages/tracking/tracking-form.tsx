650b879395a49fd86a797d5db078e30efe9feaf6
tifufu <dematasjoshua@hotmail.com>
2023-10-31 02:04:33 +0100

    Fixed bug where tracking-form was sending player data using old "id" key.

--- gui/src/pages/tracking/tracking-form.tsx
+++ gui/src/pages/tracking/tracking-form.tsx
@@ -30,8 +30,9 @@ export const TrackingForm: React.FC = () => {
     send({
       type: "submit",
       playerInfo: {
-        displayName: oldPlayers?.find((old) => old.code == playerIdInput) ?? playerIdInput,
-        id: playerIdInput,
+        displayName:
+          oldPlayers?.find((old) => old.code == playerIdInput) ?? playerIdInput,
+        code: playerIdInput,
       },
       restore: restoreRef.current && restoreRef.current.checked,
     });
@@ -81,15 +82,23 @@ export const TrackingForm: React.FC = () => {
           </div>
         )}
         <footer className="flex items-center w-full">
-          {oldPlayers && oldPlayers.some(old => old.code == playerIdInput) && (
-            <div className="group flex items-center">
-              <Checkbox ref={restoreRef} id="restore-session" />
-              <label htmlFor="restore-session" className="text-lg cursor-pointer text-gray-300 group-hover:text-white transition-colors">
-                {t("restoreSession")}
-              </label>
-            </div>
-          )}
-          <ActionButton type="submit" style={{ filter: "hue-rotate(-65deg)" }} className="ml-auto">         
+          {oldPlayers &&
+            oldPlayers.some((old) => old.code == playerIdInput) && (
+              <div className="group flex items-center">
+                <Checkbox ref={restoreRef} id="restore-session" />
+                <label
+                  htmlFor="restore-session"
+                  className="text-lg cursor-pointer text-gray-300 group-hover:text-white transition-colors"
+                >
+                  {t("restoreSession")}
+                </label>
+              </div>
+            )}
+          <ActionButton
+            type="submit"
+            style={{ filter: "hue-rotate(-65deg)" }}
+            className="ml-auto"
+          >
             {t("start")}
           </ActionButton>
         </footer>
