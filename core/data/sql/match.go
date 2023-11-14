74555a05e4bdb56783964c3a47fdf35f3ed93ed6
tifufu <dematasjoshua@hotmail.com>
2023-11-14 17:41:07 +0100

    Changed match table to use compound key since we no longer have session ids. Implemented SaveMatch function.

--- core/data/sql/match.go
+++ core/data/sql/match.go
@@ -1,10 +1,13 @@
 package sql
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+)
 
 type Match struct {
-	Id                int    `db:"id"`
-	SessionId         string `db:"session_id"`
+	UserId            uint8  `db:"user_id"`
+	SessionCreatedAt  string `db:"session_created_at"`
 	Character         string `db:"character"`
 	LP                int    `db:"lp"`
 	MR                int    `db:"mr"`
@@ -27,7 +30,40 @@ func (s *Storage) GetMatches(sessionId string) ([]*Match, error) {
 	return nil, nil
 }
 
-func (s *Storage) SaveMatch() error {
+func (s *Storage) SaveMatch(ctx context.Context, match Match) error {
+	query := `
+		INSERT OR IGNORE INTO matches (
+			user_id,
+			session_created_at,
+			character,
+			lp,
+			mr,
+			opponent,
+			opponent_character,
+			opponent_lp,
+			opponent_mr,
+			victory,
+			datetime
+		)
+		VALUES (
+			:user_id,
+			:session_created_at,
+			:character,
+			:lp,
+			:mr,
+			:opponent,
+			:opponent_character,
+			:opponent_lp,
+			:opponent_mr,
+			:victory,
+			:datetime
+		)
+	`
+	_, err := s.db.NamedExecContext(ctx, query, match)
+	if err != nil {
+		return fmt.Errorf("create match: %w", err)
+	}
+
 	return nil
 }
 
@@ -38,8 +74,8 @@ func (s *Storage) RemoveMatches(sessionId string) error {
 func (s *Storage) createMatchesTable() error {
 	_, err := s.db.Exec(`
 	CREATE TABLE IF NOT EXISTS matches (
-		id INTEGER PRIMARY KEY,
-		session_id INTEGER,
+		user_id INTEGER,
+		session_created_at TEXT,
 		character TEXT NOT NULL,
 		lp INTEGER,
 		mr INTEGER,
@@ -49,7 +85,8 @@ func (s *Storage) createMatchesTable() error {
 		opponent_mr INTEGER,
 		victory BOOLEAN,
 		datetime TEXT,
-		FOREIGN KEY(session_id) REFERENCES sessions(id)
+		PRIMARY KEY (user_id, datetime),
+		FOREIGN KEY(user_id, session_created_at) REFERENCES sessions(user_id, created_at)
 	)`)
 	if err != nil {
 		return fmt.Errorf("create users table: %w", err)
