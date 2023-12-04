309e0f059bf57a466bcef1a276ca99138808a9da
William Sjökvist <35743604+williamsjokvist@users.noreply.github.com>
2023-12-04 22:03:35 +0100

    feat: improve landing page (#76)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cfn.williamsjokvist.se/',
   base: '/',
-  integrations: [tailwind(), image()]
+  integrations: [tailwind()]
 });
