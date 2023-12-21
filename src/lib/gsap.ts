ae44299620d4727d114a8e3a0046d648f45e4add
William Sjökvist <william.sjokvist@gmail.com>
2023-12-22 00:48:32 +0100

    feat: animations

---
+++ src/lib/gsap.ts
@@ -0,0 +1,9 @@
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+if (typeof window !== "undefined") {
+  gsap.registerPlugin(ScrollTrigger);
+}
+
+export * from "gsap";
+export { ScrollTrigger };
