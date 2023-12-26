c44e7f34c73c6f46413e22c44886c0518b38f028
William Sjökvist <william.sjokvist@gmail.com>
2023-12-26 17:48:48 +0100

    fix: streamer names, scrolltriggers, download button in guide

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cfn.williamsjokvist.se/',
   base: '/',
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx()]
 });
