0d4e8d07eb1370ddda7596a6619f210220570185
tifufu <dematasjoshua@hotmail.com>
2024-01-23 16:53:07 +0100

    theme list css font style fix for chromium

--- gui/src/pages/output/theme-dialog.tsx
+++ gui/src/pages/output/theme-dialog.tsx
@@ -29,16 +29,17 @@ export const ThemeDialog: React.FC<ThemeDialogProps> = ({ selectedTheme, onSelec
   React.useEffect(() => {
     if (!isOpen || !containerRef.current) return
 
-    for (const theme of themes) {      
+    for (const theme of themes) {
       const container = document.createElement("div");
       containerRef.current.appendChild(container);
       const shadowRoot = container.attachShadow({ mode: "open" });
 
       createRoot(container).render(
         <div className="mb-4">
+          <style>{theme.css.match(/@import.*;/g)}</style>
           <Checkbox id={`${theme.name}-checkbox`} checked={theme.name === selectedTheme} onChange={(e) => onSelect(theme.name)} />
-          <label 
-            htmlFor={`${theme.name}-checkbox`} 
+          <label
+            htmlFor={`${theme.name}-checkbox`}
             style={{ fontFamily: "League Spartan" }}
             className="capitalize text-lg font-bold cursor-pointer"
           >
@@ -61,7 +62,6 @@ export const ThemeDialog: React.FC<ThemeDialogProps> = ({ selectedTheme, onSelec
       )
     }
 
-
     return () => {
       if (containerRef.current)
         containerRef.current.innerHTML = ""
@@ -69,14 +69,14 @@ export const ThemeDialog: React.FC<ThemeDialogProps> = ({ selectedTheme, onSelec
   }, [isOpen, themes, selectedTheme])
 
   return (
-    <Dialog.Root onOpenChange={setOpen}> 
+    <Dialog.Root onOpenChange={setOpen}>
       <Dialog.Trigger asChild>
         <ActionButton className="capitalize" style={{ filter: "hue-rotate(-180deg)", justifyContent: "center" }}>
           <Icon icon="ph:paint-bucket-fill" className="mr-3 w-6 h-6" />
           {selectedTheme}
         </ActionButton>
       </Dialog.Trigger>
-      <Dialog.Content title="selectTheme"> 
+      <Dialog.Content title="selectTheme">
         <div ref={containerRef} className='grid gap-4 mt-2 h-80 w-full pr-2 overflow-y-scroll'></div>
       </Dialog.Content>
     </Dialog.Root>
