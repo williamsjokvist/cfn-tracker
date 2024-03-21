0d0afbcc9bac4cf797782a6f12a6c0d2ed5e7768
William Sjökvist <35743604+williamsjokvist@users.noreply.github.com>
2024-03-21 17:13:57 +0100

    chore: use go-1.22 routing in browser src (#151)
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,11 +2,13 @@ package server
 
 import (
 	"context"
-	_ "embed"
+	"embed"
 	"encoding/json"
 	"fmt"
+	"io/fs"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	wails "github.com/wailsapp/wails/v2/pkg/runtime"
@@ -15,53 +17,30 @@ import (
 	"github.com/williamsjokvist/cfn-tracker/pkg/model"
 )
 
-//go:embed static/index.html
-var page []byte
+//go:embed static
+var staticFs embed.FS
 
-//go:embed static/themes/default.css
-var defaultCss []byte
-
-//go:embed static/themes/blades.css
-var bladesCss []byte
-
-//go:embed static/themes/jaeger.css
-var jaegerCss []byte
-
-//go:embed static/themes/nord.css
-var nordCss []byte
-
-//go:embed static/themes/pills.css
-var pillsCss []byte
+var mhJson *[]byte
 
 func GetInternalThemes() []model.Theme {
-	return []model.Theme{
-		{
-			Name: "default",
-			CSS:  string(defaultCss),
-		},
-		{
-			Name: "jaeger",
-			CSS:  string(jaegerCss),
-		},
-		{
-			Name: "nord",
-			CSS:  string(nordCss),
-		},
-		{
-			Name: "pills",
-			CSS:  string(pillsCss),
-		},
-		{
-			Name: "blades",
-			CSS:  string(bladesCss),
-		},
-	}
+	var themes = make([]model.Theme, 0, 10)
+
+	fs.WalkDir(staticFs, "static/themes", func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			return nil
+		}
+		b, _ := fs.ReadFile(staticFs, path)
+		themes = append(themes, model.Theme{
+			Name: strings.Split(d.Name(), ".css")[0],
+			CSS:  string(b),
+		})
+		return nil
+	})
+	return themes
 }
 
 func Start(ctx context.Context, cfg *config.Config) error {
 	log.Println(`Starting browser source server`)
-
-	var mhJson *[]byte
 
 	wails.EventsOn(ctx, `cfn-data`, func(incomingData ...interface{}) {
 		mh, ok := incomingData[0].(*model.TrackingState)
@@ -71,73 +50,65 @@ func Start(ctx context.Context, cfg *config.Config) error {
 		js, _ := json.Marshal(mh)
 		mhJson = &js
 	})
+	http.HandleFunc("/", handleRoot)
+	http.HandleFunc(`GET /stream`, handleStream)
+	http.HandleFunc("GET /themes/{theme}", handleTheme)
 
+	// serve custom themes through "themes" directory in the same directory as the user's executable
 	fs := http.FileServer(http.Dir("./themes"))
 	http.Handle("/themes/", http.StripPrefix("/themes/", fs))
-
-	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		w.Header().Set(`Content-Type`, `text/html`)
-		w.WriteHeader(http.StatusOK)
-		w.Write(page)
-	})
-
-	http.HandleFunc("/themes/default.css", func(w http.ResponseWriter, _ *http.Request) {
-		w.Header().Set(`Content-Type`, `text/css`)
-		w.WriteHeader(http.StatusOK)
-		w.Write(defaultCss)
-	})
-
-	http.HandleFunc("/themes/blades.css", func(w http.ResponseWriter, _ *http.Request) {
-		w.Header().Set(`Content-Type`, `text/css`)
-		w.WriteHeader(http.StatusOK)
-		w.Write(bladesCss)
-	})
-
-	http.HandleFunc("/themes/jaeger.css", func(w http.ResponseWriter, _ *http.Request) {
-		w.Header().Set(`Content-Type`, `text/css`)
-		w.WriteHeader(http.StatusOK)
-		w.Write(jaegerCss)
-	})
-
-	http.HandleFunc("/themes/nord.css", func(w http.ResponseWriter, _ *http.Request) {
-		w.Header().Set(`Content-Type`, `text/css`)
-		w.WriteHeader(http.StatusOK)
-		w.Write(nordCss)
-	})
-
-	http.HandleFunc("/themes/pills.css", func(w http.ResponseWriter, _ *http.Request) {
-		w.Header().Set(`Content-Type`, `text/css`)
-		w.WriteHeader(http.StatusOK)
-		w.Write(pillsCss)
-	})
-
-	http.HandleFunc(`/stream`, func(w http.ResponseWriter, _ *http.Request) {
-		flusher, ok := w.(http.Flusher)
-		var lastJson *[]byte = nil
-		if !ok {
-			http.Error(w, `SSE not supported`, http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set(`Content-Type`, `text/event-stream`)
-		w.Header().Set(`Cache-Control`, `no-cache`)
-		w.Header().Set(`Connection`, `keep-alive`)
-
-		ticker := time.NewTicker(time.Second * 5)
-		for range ticker.C {
-			if mhJson == lastJson {
-				continue
-			}
-
-			fmt.Fprint(w, "event: message\n\n")
-			fmt.Fprintf(w, "data: %s\n\n", *mhJson)
-			lastJson = mhJson
-			flusher.Flush()
-		}
-	})
 
 	if err := http.ListenAndServe(fmt.Sprintf(`:%d`, cfg.BrowserSourcePort), nil); err != nil {
 		return fmt.Errorf(`failed to launch browser source server: %v`, err)
 	}
 	return nil
+}
+
+func handleStream(w http.ResponseWriter, _ *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	var lastJson *[]byte = nil
+	if !ok {
+		http.Error(w, `SSE not supported`, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set(`Content-Type`, `text/event-stream`)
+	w.Header().Set(`Cache-Control`, `no-cache`)
+	w.Header().Set(`Connection`, `keep-alive`)
+
+	ticker := time.NewTicker(time.Second * 5)
+	for range ticker.C {
+		if mhJson == lastJson {
+			continue
+		}
+
+		fmt.Fprint(w, "event: message\n\n")
+		fmt.Fprintf(w, "data: %s\n\n", *mhJson)
+		lastJson = mhJson
+		flusher.Flush()
+	}
+}
+
+func handleTheme(w http.ResponseWriter, req *http.Request) {
+	fileName := req.PathValue("theme")
+	css, err := staticFs.ReadFile(fmt.Sprintf("static/themes/%s", fileName))
+
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+	} else {
+		w.Header().Set(`Content-Type`, `text/css`)
+		w.WriteHeader(http.StatusOK)
+		w.Write(css)
+	}
+}
+
+func handleRoot(w http.ResponseWriter, _ *http.Request) {
+	html, err := staticFs.ReadFile("static/index.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	} else {
+		w.Header().Set(`Content-Type`, `text/html`)
+		w.WriteHeader(http.StatusOK)
+		w.Write(html)
+	}
 }
