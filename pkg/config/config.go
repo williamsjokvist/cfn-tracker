2be4a433505589e0e349b7f41e11f2cfaa50c7a7
William Sjökvist <william.sjokvist@gmail.com>
2024-10-19 22:16:14 +0200

    chore: rm unnecessary steam creds

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,8 +4,6 @@ type Config struct {
 	AppVersion        string `envconfig:"APP_VERSION" default:"0.0.0"`
 	Headless          bool   `envconfig:"HEADLESS" default:"true"`
 	BrowserSourcePort int    `envconfig:"BROWSER_SOURCE_PORT" default:"4242"`
-	SteamUsername     string `envconfig:"STEAM_USERNAME" required:"true"`
-	SteamPassword     string `envconfig:"STEAM_PASSWORD" required:"true"`
 	CapIDEmail        string `envconfig:"CAP_ID_EMAIL" required:"true"`
 	CapIDPassword     string `envconfig:"CAP_ID_PASSWORD" required:"true"`
 }
