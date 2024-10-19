2e38cf190b75cd5b44b7e5dbed71254422d89bca
William Sjökvist <william.sjokvist@gmail.com>
2024-10-19 21:49:06 +0200

    fix default theme

--- gui/src/main/config.tsx
+++ gui/src/main/config.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
-import type { model } from '@model'
+import { model } from '@model'
 import { GetGuiConfig } from '@cmd/CommandHandler'
 
 const initialConfig: model.GuiConfig = {
   locale: 'en-GB',
-  theme: 'default',
+  theme: model.ThemeName.DEFAULT,
   sidebarMinified: false
 }
 
@@ -24,7 +24,7 @@ export function ConfigProvider({ children }: React.PropsWithChildren) {
   const [cfg, setCfg] = React.useState(initialConfig)
 
   React.useEffect(() => {
-    ;(async function () {
+    ; (async function () {
       const cfg = await GetGuiConfig()
       setCfg(cfg)
       i18n.changeLanguage(cfg.locale)
