1bbddf7ccb40c3fd6460f291d6f36b0d22326f98
William Sjökvist <william.sjokvist@gmail.com>
2024-10-21 00:05:13 +0200

    feat: restyle sessions list page

--- gui/src/pages/sessions.tsx
+++ gui/src/pages/sessions.tsx
@@ -4,10 +4,14 @@ import { useLoaderData, useNavigate } from 'react-router-dom'
 
 import * as Page from '@/ui/page'
 import * as Table from '@/ui/table'
+import { motion } from 'framer-motion'
 
 import type { model } from '@model'
+import { Button } from '@/ui/button'
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/ui/hover-card'
 
-type MonthGroup = Record<string, model.Session[]>
+type DayGroup = Record<string, model.Session[]>
+type MonthGroup = Record<string, DayGroup>
 type YearGroup = Record<string, MonthGroup>
 
 export function SessionsListPage() {
@@ -19,10 +23,12 @@ export function SessionsListPage() {
     const date = new Date(sesh.createdAt)
     const year = date.getFullYear()
     const month = date.getMonth() + 1
+    const day = date.getDate()
 
     group[year] = group[year] ?? {}
     group[year][month] = group[year][month] ?? []
-    group[year][month].push(sesh)
+    group[year][month][day] = group[year][month][day] ?? []
+    group[year][month][day].push(sesh)
 
     return group
   }, {})
@@ -32,70 +38,91 @@ export function SessionsListPage() {
       <Page.Header>
         <Page.Title>{t('sessions')}</Page.Title>
       </Page.Header>
-      <Table.Page>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.125 }}
+        className='overflow-y-scroll'
+      >
         {Object.keys(groupedSessions)
           .reverse()
           .map(year => (
-            <section key={year}>
-              <h2 className='mt-2 text-4xl'>{year}</h2>
+            <section key={year} className=''>
+              <h2 className='px-8 py-6 text-4xl font-bold'>{year}</h2>
               {Object.keys(groupedSessions[year])
                 .reverse()
                 .map(month => (
-                  <section key={month}>
-                    <h3 className='mt-2 text-2xl'>
+                  <div key={month}>
+                    <h3 className='px-8 py-4 text-xl font-bold'>
                       {Intl.DateTimeFormat(i18n.resolvedLanguage, {
                         month: 'long'
                       }).format(new Date(`2024-${Number(month) < 10 ? '0' + month : month}-01`))}
                     </h3>
-                    <Table.Content>
-                      <thead>
-                        <Table.Tr>
-                          <Table.Th className='w-[120px]'>{t('started')}</Table.Th>
-                          <Table.Th className='w-full'>{t('user')}</Table.Th>
-                          <Table.Th>{t('mrGain')}</Table.Th>
-                          <Table.Th>{t('lpGain')}</Table.Th>
-                          <Table.Th className='text-center'>{t('matchesWon')}</Table.Th>
-                          <Table.Th className='text-center'>{t('matchesLost')}</Table.Th>
-                        </Table.Tr>
-                      </thead>
-                      <tbody>
-                        {groupedSessions[year][month].map(sesh => (
-                          <Table.Tr
-                            key={sesh.id}
-                            className='group cursor-pointer'
-                            onClick={() => navigate(`/sessions/${sesh.id}/matches`)}
-                          >
-                            <Table.Td>
-                              <time dateTime={sesh.createdAt}>
-                                {new Date(sesh.createdAt).toLocaleDateString(
-                                  i18n.resolvedLanguage,
-                                  {
-                                    day: 'numeric',
-                                    weekday: 'short',
-                                    hour: '2-digit',
-                                    minute: '2-digit'
-                                  }
-                                )}
-                              </time>
-                            </Table.Td>
-                            <Table.Td>{sesh.userName}</Table.Td>
-                            <Table.Td className='text-center'>
-                              {`${sesh.mrGain > 0 ? `+` : ``}${sesh.mrGain}`}
-                            </Table.Td>
-                            <Table.Td className='text-center'>
-                              {`${sesh.lpGain > 0 ? `+` : ``}${sesh.lpGain}`}
-                            </Table.Td>
-                            <Table.Td className='text-center'>{sesh.matchesWon}</Table.Td>
-                            <Table.Td className='text-center'>{sesh.matchesLost}</Table.Td>
-                          </Table.Tr>
-                        ))}
-                      </tbody>
-                    </Table.Content>
-                  </section>
+                    <div
+                      style={{
+                        background: `repeating-linear-gradient(
+                          90deg,
+                          transparent 31.5px,
+                          transparent 224px,
+                          rgba(255, 255, 255, 0.125) 225px
+                        )`
+                      }}
+                      className='relative flex flex-wrap items-stretch border-y-[0.5px] border-solid border-divider px-8'
+                    >
+                      {Object.keys(groupedSessions[year][month]).map(day => (
+                        <div className='flex w-[193.5px] flex-col border-b-[0.5px] border-solid border-divider px-2'>
+                          <span className='text-center text-xl font-bold'>{day}</span>
+                          {groupedSessions[year][month][day].reverse().map(s => (
+                            <HoverCard>
+                              <HoverCardTrigger>
+                                <Button
+                                  key={day}
+                                  className='mb-1 w-full !justify-between gap-2 rounded-xl !px-[6px] !py-0 !pt-[2px] text-xl'
+                                  onClick={() => navigate(`/sessions/${s.id}/matches`)}
+                                >
+                                  <span className='text-base font-bold'>
+                                    {Intl.DateTimeFormat(i18n.resolvedLanguage, {
+                                      hour: '2-digit',
+                                      minute: '2-digit'
+                                    }).format(new Date(s.createdAt))}
+                                  </span>
+                                  <span className='text-base font-light'>{s.userName}</span>
+                                </Button>
+                              </HoverCardTrigger>
+                              <HoverCardContent side='bottom'>
+                                <dl>
+                                  <div className='flex justify-between gap-2'>
+                                    <dt>{t('wins')}</dt>
+                                    <dd>{s.matchesWon}</dd>
+                                  </div>
+                                  <div className='flex justify-between gap-2'>
+                                    <dt>{t('losses')}</dt>
+                                    <dd>{s.matchesLost}</dd>
+                                  </div>
+                                  {s.lpGain != 0 && s.mrGain != 0 && (
+                                    <>
+                                      <div className='flex justify-between gap-2'>
+                                        <dt>{t('mrGain')}</dt>
+                                        <dd>{s.mrGain}</dd>
+                                      </div>
+                                      <div className='flex justify-between gap-2'>
+                                        <dt>{t('lpGain')}</dt>
+                                        <dd>{s.lpGain}</dd>
+                                      </div>
+                                    </>
+                                  )}
+                                </dl>
+                              </HoverCardContent>
+                            </HoverCard>
+                          ))}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
                 ))}
             </section>
           ))}
-      </Table.Page>
+      </motion.div>
     </Page.Root>
   )
 }
