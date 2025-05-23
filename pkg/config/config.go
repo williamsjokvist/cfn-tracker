baa6037d6041c79bdb25da770d2924c6f1242017
William Sjökvist <35743604+williamsjokvist@users.noreply.github.com>
2025-05-23 16:46:41 +0200

    chore: use app version from config (#278)

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,7 +1,7 @@
 package config
 
 type BuildConfig struct {
-	AppVersion        string `envconfig:"APP_VERSION" default:"0.0.0"`
+	AppVersion        string `default:"1.0.0"`
 	Headless          bool   `envconfig:"HEADLESS" default:"true"`
 	BrowserSourcePort int    `envconfig:"BROWSER_SOURCE_PORT" default:"4242"`
 	CapIDEmail        string `envconfig:"CAP_ID_EMAIL" required:"true"`
